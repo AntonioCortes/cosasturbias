89568cad1f98ab1652f3947690dc5e9a092a628a
Flavius Stan <flaviusns@outlook.com>
2018-04-14 21:22:21 +0200

    Cambios

--- PECL2/PECL2APA/src/main/Main.scala
+++ PECL2/PECL2APA/src/main/Main.scala
@@ -12,7 +12,6 @@ object Main  extends App
   val numColores = asignarNumColores(dificultad)
   val numVidas = 5
   val tablero = crearLista(numFilas * numColumnas, numColores)
-    
   juego(tablero, numFilas, numColumnas, dificultad, numVidas)
   
   def poner(pos:Int,color:Int,tablero:List[Int]): List[Int] = {
@@ -108,20 +107,31 @@ object Main  extends App
 
 	def juego(tablero: List[Int],numFilas: Int, numColumnas: Int, dificultad: Int, numVidas: Int): Unit =
 	{
+	  //val mPunt= mejorJugada(tablero, 0, 0, numColumnas,0, 0)
 	  println()
 		print(s"Vidas: $numVidas Dificultad: $dificultad\n")
 		dibujarTablero(tablero, numColumnas, 0, 0, true)
+		//print(s"La máquina recomienda como mejor posición: $mPunt\n")
 		val columna = pedirColumna(numColumnas)
 		val fila = pedirFila(numFilas)
 		val pos= fila*numColumnas+columna
 		val listpos= List[Int]()
-		val listaMuerte =  buscarIguales(tablero, pos, columna, numColumnas, listpos)
+		val listaMuerte =  buscarIguales(tablero, pos, columna, numColumnas,listpos)
 		println(listaMuerte)
 		val tableroFin=eliminarIguales(tablero, listaMuerte)
 		dibujarTablero(tableroFin, numColumnas, 0, 0, true)
 		juego(tableroFin, numFilas, numColumnas, dificultad, numVidas)
   }
 	
+	
+	def mejorJugada(tablero:List[Int],pos:Int,columna:Int,width:Int,mPuntacion:Int,mPos:Int):Int={
+	  if (pos+2>tablero.length) mPos
+	  else{
+	    val puntAux=buscarIguales(tablero, pos, columna, width, List[Int]())
+	    if(puntAux.length<mPuntacion) mejorJugada(tablero, pos+1, columna, width, mPuntacion, mPos)
+	    else mejorJugada(tablero, pos+1, columna, width, puntAux.length, pos)
+	  }
+	}
 	
 	def buscarIguales(tablero:List[Int],pos:Int,columna:Int,width:Int,lpos:List[Int]):List[Int] = {
 	  //busca en todas direcciones
@@ -151,8 +161,8 @@ object Main  extends App
 	}
 	
 	def buscarIgualesAbajo(tablero:List[Int],pos:Int,columna:Int,width:Int,lpos:List[Int]):List[Int] = {
-	  if((pos<tablero.length) && (tablero(pos)==tablero(pos-width))){
-	    val abajo=buscarIgualesAbajo(tablero,pos+width,columna,width,lpos)
+	  if((pos<tablero.length-width) && (tablero(pos)==tablero(pos-width))){
+	    val abajo=buscarIgualesAbajo(tablero, pos+width, columna, width,  lpos)
 	    val derecha=buscarIgualesDerecha(tablero,pos+1,columna+1,width,lpos)
 	    val izquierda=buscarIgualesIzquierda(tablero,pos-1,columna-1,width,lpos)
 	    val lposaux=unirListas( lpos,(unirListas(abajo, (unirListas(derecha, izquierda)))))
@@ -165,14 +175,15 @@ object Main  extends App
 	}
 	
 	def buscarIgualesDerecha(tablero:List[Int],pos:Int,columna:Int,width:Int,lpos:List[Int]):List[Int] = {
-	  if((columna<=width) && (tablero(pos)==tablero(pos-1))){
-	    val arriba=buscarIgualesArriba(tablero,pos-width,columna,width,lpos)
+	  if((columna<width) && (tablero(pos)==tablero(pos-1))){//pos-width
+	    val arriba=buscarIgualesArriba(tablero, pos-width, columna, width,lpos)
 	    val abajo=buscarIgualesAbajo(tablero,pos+width,columna,width,lpos)
 	    val derecha=buscarIgualesDerecha(tablero,pos+1,columna+1,width,lpos)
 	    val lposaux=unirListas( lpos,unirListas(arriba, (unirListas(abajo,derecha))))
 	    val lposaux1= pos::lposaux
 	    return lposaux1
 	  }
+	  else if((columna==width-1) && (tablero(pos)==tablero(pos-1))) pos::lpos
 	  else{
 	    return List[Int]()
 	  }
@@ -187,6 +198,7 @@ object Main  extends App
 	    val lposaux1= pos::lposaux
 	    return lposaux1
 	  }
+	  else if(columna==0) pos::lpos
 	  else{
 	    return List[Int]()
 	  }
